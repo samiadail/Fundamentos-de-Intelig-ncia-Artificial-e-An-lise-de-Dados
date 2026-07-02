58cee551510cf5da2fb35e6231841cdec24dd034
samiadail <samiadail12@gmail.com>
2026-07-02 14:27:07 -0300

    feat: Projetos em python e ipynb

---
+++ aula01.py
@@ -0,0 +1,13 @@
+# O comando print, exibe uma informação na tela do usuário.
+
+idade = 30          # Idade um
+idade_dois = "30"   # Idade dois
+
+print(idade)
+print(idade_dois)
+
+idade_futura = idade + 1
+print(idade_futura)
+
+print(type(idade))
+print(type(idade_dois))
